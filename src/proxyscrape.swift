d7e67f032859fb7a0e97d5bbc048fec4d819ab31
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-05-19 13:21:53 +0300

    Create proxyscrape.swift

---
+++ src/proxyscrape.swift
@@ -0,0 +1,48 @@
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+extension URLSession {
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        return try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else if let data = data, let response = response {
+                    continuation.resume(returning: (data, response))
+                } else {
+                    continuation.resume(throwing: URLError(.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
+public class Proxyscrape{
+    private let api = "https://api.proxyscrape.com/v3"
+    private var headers: [String: String]
+    
+    public init() {
+        self.headers = [
+        "Connection":"keep-alive",
+        "Accept-Encoding":"deflate, zstd",
+        "Accept-Language":"en-US,en;q=0.9",
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
+        ]
+
+    }
+
+    public func get_servers_list(country: String,protocol_type: String,timeout: Int = 100,anonymity: String = "elite%2Canonymous") async throws -> Any {
+        let urlString = "\(api)/free-proxy-list/get?request=displayproxies&country=\(country)&protocol=\(protocol_type)&format=json&timeout=\(timeout)&anonymity=\(anonymity)"
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "Invalid URL", code: -1)
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONSerialization.jsonObject(with: data)
+    }
+}
